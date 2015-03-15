a1d1a184f6c1ad2b9000ac883e454678d984c34a
Tamas Piros <me@tamas.io>
2015-03-15 20:38:21 +0100

    initial state

--- app.js
+++ app.js
@@ -4,18 +4,6 @@ var express    = require('express');
 var app        = express();
 var router     = express.Router();
 
-var marklogic  = require('marklogic');
-
-var connection = {
-	host: 'localhost',
-	port: 5060,
-	user: 'admin',
-	password: 'admin'
-};
-
-var db         = marklogic.createDatabaseClient(connection);
-var qb         = marklogic.queryBuilder;
-
 app.set('port', 8080);
 app.set('view engine', 'jade');
 
@@ -31,40 +19,7 @@ var index = function(req, res) {
 	res.render('index');
 };
 
-var listAll = function(req, res) {
-	db.documents.query(qb.where(qb.collection('bio')).slice(1,20))
-	.result()
-	.then(function(documents) {
-		res.json(documents);
-	});
-};
-
-var listOne = function(req, res) {
-	var name = req.params.name;
-	var uri = '/norsegods/' + name + '.json';
-	db.documents.read(uri)
-	.result()
-	.then(function(document) {
-		res.json(document);
-	});
-};
-
-var displayImage = function(req, res) {
-	var name = req.params.name;
-	var uri = '/norsegods/' + name + '.jpg';
-	db.documents.read(uri)
-	.result()
-	.then(function(document) {
-		//new Buffer(document[0].content, 'binary').toString('base64');
-		//res.json(document);
-		res.json(new Buffer(document[0].content, 'binary').toString('base64'));
-	});
-};
-
 router.route('/').get(index);
-router.route('/api').get(listAll);
-router.route('/api/:name').get(listOne);
-router.route('/api/image/:name').get(displayImage);
 
 //route declaration for the partials
 var partials = function partials(req, res) {
--- public/app.js
+++ public/app.js
@@ -2,36 +2,13 @@
   'use strict';
   angular
     .module('norsegods', ['ngRoute'])
-    .config(config)
-    .controller('GodsController', GodsController)
-    .controller('GodController', GodController);
+    .config(config);
 
     function config($routeProvider) {
       $routeProvider
       .when('/', {
-        templateUrl: 'views/main',
-        controller: 'GodsController'
+        templateUrl: 'views/main'
       })
-      .when('/character/:name', {
-        templateUrl: 'views/character',
-        controller: 'GodController'
-      });
-    }
-
-    function GodsController($scope, $http) {
-      $http.get('/api').then(function(result) {
-        $scope.gods = result.data;
-      });
-    }
-
-    function GodController($scope, $routeParams, $http) {
-      var name = $routeParams.name;
-      $http.get('/api/' + name).then(function(result) {
-        $http.get('/api/image/' + name).then(function(binary) {
-          $scope.image = binary.data;
-          $scope.god = result.data
-        });
-      });
     }
 
 })();
